5cc937c6cc806793ce8a584ee7198fb7672d761c
thomashedgepath <thomas.hedgepath@gmail.com>
2021-04-06 21:26:22 -0500

    Moved KeyFacts to new component

---
+++ packages/mars-theme/src/components/property-page/key_facts.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { styled } from "frontity";
+
+function KeyFacts({ data }) {
+  const key_facts = data;
+  let table = [];
+
+  for (const [key, value] of Object.entries(key_facts)) {
+    // console.log(key);
+    let section = [];
+    const section_title = toCapitalizedWords(key);
+
+    const subsection = value;
+    let items = [];
+
+    for (const [sub_key, sub_value] of Object.entries(subsection)) {
+      if (sub_value !== "" && sub_value !== false) {
+        const item_name = toCapitalizedWords(sub_key);
+        items.push([[item_name, sub_value]]);
+        //console.log(section);
+      }
+    }
+    if (items.length > 0) {
+      section = [section_title, items];
+      table.push(section);
+    }
+  }
+
+    // For converting JSON Keys to Human Readable text
+    function toCapitalizedWords(name) {
+      var words = name.match(/[A-Za-z][a-z]*/g) || [];
+  
+      return words.map(capitalize).join(" ");
+    }
+  
+    function capitalize(word) {
+      return word.charAt(0).toUpperCase() + word.substring(1);
+    }
+  
+
+  return (
+    <>
+      {table.map((item, index) => {
+        var section_heading = item[0];
+        var section_data = item[1];
+
+        return (
+          <>
+            <h3 key={`section-${index}`}>{section_heading}</h3>
+
+            {section_data.map((item, index) => {
+              var attribute_name = item[0][0];
+              var attribute_value = item[0][1];
+              return (
+                <p key={`attribute-${index}`}>
+                  {attribute_name}: {attribute_value}
+                </p>
+              );
+            })}
+          </>
+        );
+      })}
+    </>
+  );
+}
+
+export default KeyFacts;
