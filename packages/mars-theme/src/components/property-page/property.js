8562b05cda8faee79930d6b412045211e8a0ad4d
thomashedgepath <thomas.hedgepath@gmail.com>
2021-04-12 17:01:09 -0500

    Page Layout almost complete

--- packages/mars-theme/src/components/property-page/property.js
+++ packages/mars-theme/src/components/property-page/property.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
-import { connect, styled } from "frontity";
+import React, { useEffect, useState } from "react";
+import { connect, Head, styled } from "frontity";
 import List from "../list";
 import list from "../list/list";
 import KeyFacts from "./key_facts";
 import ImageSection from "./image_section";
 import PricingSection from "./pricing_section";
+import MapGL, { NavigationControl } from "react-map-gl";
+import MapMarker from "../properties-page/map-marker";
+import { Button, Divider, Header, Table } from "semantic-ui-react";
+import { TableCell } from "@material-ui/core";
 
 const Property = ({ state, actions }) => {
   // Get information about the current URL.
@@ -23,59 +27,135 @@ const Property = ({ state, actions }) => {
     List.preload();
   }, []);
 
+  const accessToken =
+    "pk.eyJ1IjoidGhvbWFzaGVkZ2VwYXRoIiwiYSI6ImNqNW83cXhsYjQyYnAyd25xbjdwbG1xbmoifQ.u8oSi903x7-iK9S0-lNuNg";
+  const [viewport, setViewport] = useState({
+    latitude: parseFloat(data.address.lat),
+    longitude: parseFloat(data.address.lng),
+    zoom: 14,
+    bearing: 0,
+    pitch: 0,
+  });
+
   // Load the post, but only if the data is ready.
   return (
     <>
       {console.log(data)}
       <ContentContainer>
-        <h1>Images</h1>
-        <ImageSection data={data.images}></ImageSection>
+        <MainColumn>
+          <MainContent>
+            <div style={{ margin: "27px 0" }}>
+              <PageHeader>{data.property_name}</PageHeader>
+              <Address>{`${data.address.street_address} ${data.address.city}, ${data.address.state} ${data.address.zip_code}`}</Address>
+            </div>
 
-        <h1>Headers</h1>
-        <p>{data.property_name}</p>
-        <p>{data.headers.primary}</p>
-        <p>{data.headers.secondary}</p>
+            <ImageSection data={data.images}></ImageSection>
 
-        <h1>Address</h1>
-        <p>
-          {`${data.address.street_address} ${data.address.city}, ${data.address.state} ${data.address.zip_code}`}
-        </p>
+            <SecondaryHeader>{data.headers.secondary}</SecondaryHeader>
+            <PageHeader>{data.headers.primary}</PageHeader>
+            <Divider horizontal>
+              <Header as="h4">Property Description</Header>
+            </Divider>
+            <DescriptionText
+              dangerouslySetInnerHTML={{ __html: data.property_description }}
+            ></DescriptionText>
+            <Divider horizontal>
+              <Header as="h4">Key Facts</Header>
+            </Divider>
+            <KeyFacts data={data.key_facts}></KeyFacts>
+            <Divider horizontal>
+              <Header as="h4">Location</Header>
+            </Divider>
+            <MapContainer>
+              <MapGL
+                {...viewport}
+                width="100%"
+                height="100%"
+                mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
+                onViewportChange={(nextViewport) => setViewport(nextViewport)}
+                mapboxApiAccessToken={accessToken}
+              >
+                <NavControls />
+                <MapMarker coordinates={[data.address.lat, data.address.lng]} />
+              </MapGL>
+            </MapContainer>
+          </MainContent>
+        </MainColumn>
+        <SideColumn>
+          <MainContent>
+            <div style={{ margin: "27px 0", textAlign: "center" }}>
+              <SidebarHeader>
+                {data.status.for_sale ? (
+                  <>{`${data.listing_type[0]} `}For Sale</>
+                ) : (
+                  <></>
+                )}
+              </SidebarHeader>
+              <SidebarHeader>
+                {data.status.for_lease ? (
+                  <>{`${data.listing_type[0]} `}For Lease</>
+                ) : (
+                  <></>
+                )}
+              </SidebarHeader>
+            </div>
 
-        <h1>Listing Type</h1>
-        <p>
-          {/* this could be multiple values need to map */}
-          {`${data.listing_type[0]}`}
-        </p>
+            <Divider horizontal>
+              <Header as="h4">Availability</Header>
+            </Divider>
+            <Table>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Total Availability</Table.HeaderCell>
+                  <Table.HeaderCell>
+                    {data.availability.total_availability}{" "}
+                    {data.availability.units}
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
 
-        <h1>Status</h1>
-        {data.status.for_sale ? <>For Sale</> : <></>}
-        {data.status.for_lease ? <>For Lease</> : <></>}
+              {data.availability.min_divisible !== "" ? (
+                <Table.Row>
+                  <Table.Cell>Min Divisible</Table.Cell>
+                  <Table.Cell>
+                    {data.availability.min_divisible} {data.availability.units}
+                  </Table.Cell>
+                </Table.Row>
+              ) : (
+                <></>
+              )}
+              {data.availability.max_contig !== "" ? (
+                <Table.Row>
+                  <Table.Cell>Max Contig</Table.Cell>
+                  <Table.Cell>
+                    {data.availability.max_contig} {data.availability.units}
+                  </Table.Cell>
+                </Table.Row>
+              ) : (
+                <></>
+              )}
+            </Table>
 
-        <h1>Availability</h1>
-        <p>
-          Total Availability: {data.availability.total_availability}{" "}
-          {data.availability.units}
-        </p>
-        <p>Max Contig: {data.availability.max_contig}</p>
-        <p>Min Divisible: {data.availability.min_divisible}</p>
+            <Divider horizontal>
+              <Header as="h4">Pricing</Header>
+            </Divider>
 
-        <h1>Pricing</h1>
-        <PricingSection data={data.pricing} status={data.status}></PricingSection>
+            <PricingSection
+              data={data.pricing}
+              status={data.status}
+            ></PricingSection>
 
-        <h1>Video</h1>
-
-        <h1>Brochure</h1>
-        <a href={data.brochure.link} target="_blank" rel="noopener noreferrer">
-          Brochure
-        </a>
-
-        <h1>Description</h1>
-        <div
-          dangerouslySetInnerHTML={{ __html: data.property_description }}
-        ></div>
-
-        <h1>Key Facts</h1>
-        <KeyFacts data={data.key_facts}></KeyFacts>
+            <Button>
+              <a
+                href={data.brochure.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Brochure
+              </a>
+            </Button>
+          </MainContent>
+        </SideColumn>
       </ContentContainer>
     </>
   );
@@ -83,24 +163,13 @@ const Property = ({ state, actions }) => {
 
 export default connect(Property);
 
-const SectionContainer = styled.div`
-  position: relative;
-  overflow: hidden;
-  flex-wrap: none;
-  width: 100vw;
-  height: 100%;
-
-  background-color: #f1f5f2;
-`;
-
+//Layout Styles
 const ContentContainer = styled.div`
-  ${
-    "" /* display: grid;
-  grid-template-columns: 40vw 40vw;
+  display: grid;
+  grid-template-columns: 15vw 60vw 20vw 5vw;
+  gap: 10px;
   justify-content: center;
-
   color: #007a0f;
-
   margin: 0;
   font-family: "Overpass", sans-serif;
   letter-spacing: -1px;
@@ -108,103 +177,88 @@ const ContentContainer = styled.div`
   @media (max-width: 768px) {
     display: block;
     letter-spacing: -0.5px;
-  } */
   }
 `;
-
-const LContent = styled.div`
-  grid-column-start: 1;
-
+const MainColumn = styled.div`
+  grid-column-start: 2;
   display: flex;
-
   font-weight: 500;
   text-align: left;
-  font-size: 4vw;
-
+  font-size: 1vw;
   align-content: center;
   align-items: center;
   justify-content: center;
-
   background-color: none;
+  margin: 10px 0 10px 0;
 
   @media (max-width: 768px) {
   }
 `;
-
-const RContent = styled(LContent)`
-  grid-column-start: 2;
+const SideColumn = styled(MainColumn)`
+  grid-column-start: 3;
+  align-content: left;
+  align-items: start;
   display: flex;
   background-color: none;
-  border-left: 4px solid #2b4f77;
+
+  ${"" /* border-left: 4px solid #2b4f77; */}
 
   @media (max-width: 768px) {
   }
 `;
-
-const ProductImage = styled.img`
-  width: 20vw;
-  height: auto;
-  object-fit: cover;
-  filter: drop-shadow(0px 3px 3px rgba(0, 0, 0, 0.5));
-  margin: 2vh 0;
-
-  margin-left: ${(props) => (props.right ? "0" : "auto")};
-
-  @media (max-width: 768px) {
-    height: 40vh;
-  }
+const MainContent = styled.div`
+  display: block !important;
+  background-color: white;
+  padding: 0 20px 0 20px;
+  border-radius: 4px;
+  width: 100%;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
 `;
 
-const PrimaryText = styled.div`
-  text-align: left;
-  font-family: "Overpass", sans-serif;
-  font-size: 6vh;
-  font-weight: 300;
+// Text Styles
+const PageHeader = styled.h1`
+  font-size: 2vw;
+  line-height: 1.75vw;
+  text-transform: uppercase;
+  color: #991a1e;
+`;
+const Address = styled.h2`
+  font-size: 1.5vw;
+  line-height: 0;
+  text-transform: uppercase;
+  color: #2e5077;
+`;
+const SecondaryHeader = styled.h3`
+  font-size: 1vw;
+  line-height: 0.5vw;
+  text-transform: uppercase;
+  color: #666766;
+`;
+const SidebarHeader = styled.h3`
+  font-size: 2vw;
+  line-height: 0.5vw;
+  text-transform: uppercase;
+  color: #991a1e;
+`;
+const SectionTitle = styled.h3`
+  font-size: 2vh;
   color: black;
-  letter-spacing: -3pt;
+`;
+const DescriptionText = styled.p`
+  font-size: 2vh;
+  color: black;
+`;
+
+// Map Styles
+const MapContainer = styled.div`
+  background-color: grey;
+  flex-grow: 1;
+  height: 60vh;
   margin: 10px;
-  line-height: 0.85;
-
-  @media (max-width: 768px) {
-    font-size: 5vh;
-    margin: 20px;
-  }
 `;
-
-const SecondaryText = styled(PrimaryText)`
-  font-size: 3vh;
-  color: #858585;
-  font-weight: 200;
-  line-height: 1.2;
-  letter-spacing: -1px;
-
-  @media (max-width: 768px) {
-    font-size: 5.5vw;
-  }
-`;
-
-const Circle = styled.div`
-  width: 20px;
-  height: 20px;
-  background: #2b4f77;
-  border-radius: 50%;
-  margin-left: auto;
-  margin-right: -12px;
-  right: 0;
-`;
-
-const TextCard = styled.div`
-  text-align: center;
-  font-size: 1.8vh;
-  width: 80%;
-  ${"" /* background-color: white; */}
-  margin: 20px;
-  border-radius: 20px;
-  padding: 1vw;
-  ${"" /* filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.5)); */}
-
-  @media (max-width: 768px) {
-    margin: -10vh 0 10px 0;
-    width: 90%;
-  }
+const NavControls = styled(NavigationControl)`
+  position: relative;
+  float: right;
+  margin: 10px;
 `;
