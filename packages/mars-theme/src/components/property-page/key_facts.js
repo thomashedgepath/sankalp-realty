4452d6ece14fef240c5013b108fc1fd5a1f34cb9
thomashedgepath <thomas.hedgepath@gmail.com>
2021-04-12 16:59:06 -0500

    Cleaned up using semantic ui Table

--- packages/mars-theme/src/components/property-page/key_facts.js
+++ packages/mars-theme/src/components/property-page/key_facts.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { styled } from "frontity";
+import { Icon, Label, Menu, Table } from "semantic-ui-react";
 
 function KeyFacts({ data }) {
   const key_facts = data;
@@ -26,40 +27,50 @@ function KeyFacts({ data }) {
     }
   }
 
-    // For converting JSON Keys to Human Readable text
-    function toCapitalizedWords(name) {
-      var words = name.match(/[A-Za-z][a-z]*/g) || [];
-  
-      return words.map(capitalize).join(" ");
-    }
-  
-    function capitalize(word) {
-      return word.charAt(0).toUpperCase() + word.substring(1);
-    }
-  
+  // For converting JSON Keys to Human Readable text
+  function toCapitalizedWords(name) {
+    var words = name.match(/[A-Za-z][a-z]*/g) || [];
+
+    return words.map(capitalize).join(" ");
+  }
+
+  function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+  }
 
   return (
     <>
-      {table.map((item, index) => {
-        var section_heading = item[0];
-        var section_data = item[1];
+      <Table celled>
+        {table.map((item, index) => {
+          var section_heading = item[0];
+          var section_data = item[1];
 
-        return (
-          <>
-            <h3 key={`section-${index}`}>{section_heading}</h3>
+          return (
+            <>
+              <Table.Header key={`section-${index}`}>
+                <Table.Row>
+                  <Table.HeaderCell colSpan="2">
+                    {section_heading}
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
 
-            {section_data.map((item, index) => {
-              var attribute_name = item[0][0];
-              var attribute_value = item[0][1];
-              return (
-                <p key={`attribute-${index}`}>
-                  {attribute_name}: {attribute_value}
-                </p>
-              );
-            })}
-          </>
-        );
-      })}
+              <Table.Body>
+                {section_data.map((item, index) => {
+                  var attribute_name = item[0][0];
+                  var attribute_value = item[0][1];
+                  return (
+                    <Table.Row key={`attribute-${index}`}>
+                      <Table.Cell>{attribute_name}</Table.Cell>
+                      <Table.Cell>{attribute_value}</Table.Cell>
+                    </Table.Row>
+                  );
+                })}
+              </Table.Body>
+            </>
+          );
+        })}
+      </Table>
     </>
   );
 }
