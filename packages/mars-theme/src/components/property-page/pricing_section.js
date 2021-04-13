cfbe5b59df3c028b8d23a1c7299d0375055bda01
thomashedgepath <thomas.hedgepath@gmail.com>
2021-04-13 09:14:56 -0500

    logic added for showing sale and lease rates

--- packages/mars-theme/src/components/property-page/pricing_section.js
+++ packages/mars-theme/src/components/property-page/pricing_section.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { styled } from "frontity";
+import { Button, Divider, Header, Table } from "semantic-ui-react";
 
 function PricingSection({ data, status }) {
   // Create our number formatter.
@@ -15,45 +16,86 @@ function PricingSection({ data, status }) {
     return formatter.format(number);
   };
 
+  let showSale = false;
+  let showLease = false;
+
+  if (status.hide_price !== true) {
+    if (data.sale_price.min !== undefined && data.sale_price.min > 0) {
+      showSale = true;
+    }
+    if (data.lease_rate.min !== undefined && data.lease_rate.min > 0) {
+      showLease = true;
+    }
+  }
+
   return (
     <>
-      {/* {console.log(data)} */}
-      {/*  Move logic to display this section out of the render: hide price || all rates = 0  */}
-      {status.hide_price ? (
-        <>Call for Current Pricing</>
-      ) : (
-        <>
-          {/* For Sale Pricing Section */}
-          <h3>Sale</h3>
-          {status.for_sale ? (
-            <>
-              {formatMoney(data.sale_price.min)}
-              {data.sale_price.max
-                ? ` - ${formatMoney(data.sale_price.max)} `
-                : " "}
-              {data.sale_price.label}
-            </>
-          ) : (
-            <></>
-          )}
+      {console.log(showSale)}
+      {console.log(showLease)}
 
-          <br></br>
-          
-          {/* For Lease Pricing Section */}
-          <h3>Lease</h3>
-          {status.for_lease ? (
-            <>
-              {formatMoney(data.lease_rate.min)}
-              {data.lease_rate.max
-                ? ` - ${formatMoney(data.lease_rate.max)} `
-                : " "}
-              {data.lease_rate.label}
-            </>
-          ) : (
-            <></>
-          )}
-        </>
-      )}
+      <>
+        {/* For Sale Pricing Section */}
+        {/* {showSale ? (<></>):(<>Call for Current Sale Price</>)} */}
+
+        {status.for_sale && showSale ? (
+          <>
+            <Divider horizontal>
+              <Header as="h4">Sale Price</Header>
+            </Divider>
+            {formatMoney(data.sale_price.min)}
+            {data.sale_price.max
+              ? ` - ${formatMoney(data.sale_price.max)} `
+              : " "}
+            {data.sale_price.label}
+          </>
+        ) : (
+          <>
+            {status.for_sale ? (
+              <>
+                <Divider horizontal>
+                  <Header as="h4">Sale Price</Header>
+                </Divider>
+                Call for Current Sale Prices
+              </>
+            ) : (
+              <></>
+            )}
+          </>
+        )}
+
+        <br></br>
+
+        {/* For Lease Pricing Section */}
+
+        {/* {showLease ? (<></>):(<>Call for Current Lease Rates</>)} */}
+
+        {status.for_lease && showLease ? (
+          <>
+            <Divider horizontal>
+              <Header as="h4">Lease Rates</Header>
+            </Divider>
+            {formatMoney(data.lease_rate.min)}
+            {data.lease_rate.max
+              ? ` - ${formatMoney(data.lease_rate.max)} `
+              : " "}
+            {data.lease_rate.label}
+          </>
+        ) : (
+          <>
+            {status.for_lease ? (
+              <>
+                <Divider horizontal>
+                  <Header as="h4">Lease Rates</Header>
+                </Divider>
+                Call for Current Lease Rates
+              </>
+            ) : (
+              <></>
+            )}
+          </>
+        )}
+      </>
+      <br></br>
     </>
   );
 }
